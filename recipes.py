f1bb860c9239b7f65ba541d6b774efd84f6d21fc
Прохоров Михаил <mikmikprokhorov@edu.hse.ru>
2026-06-03 13:42:18 +0300

    Исправлена аннотация в Ingredient для quantity.setter
    Сделан класс Recipe в файле recipes.py
    - сделана проверка в scale на валидацию ratio
    - сделана проверка на уникальность в __len__
    
    Добавлена в README.md заметка про тонкости написания

--- recipes.py
+++ recipes.py
@@ -7,7 +7,7 @@ class Ingredient:
     def quantity(self) -> float:
         return self._quantity
     @quantity.setter
-    def quantity(self, value: object) -> None:
+    def quantity(self, value: float | int | str) -> None:
         value_float = float(value)
         if value_float > 0:
             self._quantity = value_float
@@ -21,3 +21,41 @@ class Ingredient:
         if not isinstance(other, Ingredient):
             return False
         return self.name == other.name and self.unit == other.unit
+
+class Recipe:
+    def __init__(self, title: str, ingredients: list[Ingredient]) -> None:
+        self.title = title
+        self.ingredients = ingredients
+    def add_ingredient(self, ingredient: Ingredient) -> None:
+        if ingredient in self.ingredients:
+            self.ingredients[self.ingredients.index(ingredient)].quantity += ingredient.quantity
+        else:
+            self.ingredients.append(ingredient)
+    @staticmethod
+    def is_valid_ratio(ratio) -> bool:
+        return isinstance(ratio, (int, float)) and ratio > 0
+    def scale(self, ratio) -> 'Recipe':
+        # не говорилось про проверку, но включил
+        if not self.is_valid_ratio(ratio):
+            raise ValueError("Некорректный коэффициент масштабирования")
+        return self.__class__(
+            self.title,
+            [
+                Ingredient(
+                    ingredient.name,
+                    ingredient.quantity * ratio,
+                    ingredient.unit
+                )
+
+                for ingredient in self.ingredients
+            ]
+        )
+    def __len__(self) -> int:
+        # вдруг при __init__ был задан не правильно, а в ТЗ написано "уникальные"
+        uni_ingredients = []
+        for ingredient in self.ingredients:
+            if ingredient not in uni_ingredients:
+                uni_ingredients.append(ingredient)
+        return len(uni_ingredients)
+    def __str__(self) -> str:
+        return f"{self.title}:\n" + "\n".join(str(ingredient) for ingredient in self.ingredients)
