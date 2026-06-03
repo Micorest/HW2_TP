6a540492bea73d903bc66a4995b6569c4c64fbcb
Прохоров Михаил <mikmikprokhorov@edu.hse.ru>
2026-06-03 14:23:10 +0300

    Сделан класс ShoppingList в файле recipes.py - в __add__ ShoppingList проверяется соотвествие типов
    Добавлена в README.md заметка про тонкости написания

--- recipes.py
+++ recipes.py
@@ -59,3 +59,40 @@ class Recipe:
         return len(uni_ingredients)
     def __str__(self) -> str:
         return f"{self.title}:\n" + "\n".join(str(ingredient) for ingredient in self.ingredients)
+
+class ShoppingList:
+    def __init__(self) -> None:
+        self._items: list[tuple[Ingredient, str]] = []
+    def add_recipe(self, recipe: Recipe, portions: float) -> None:
+        if portions <= 0:
+            raise ValueError("Количество порций должно быть положительным")
+        scaled_recipe = recipe.scale(portions)
+        for ingredient in scaled_recipe.ingredients:
+            self._items.append((ingredient, scaled_recipe.title))
+    def remove_recipe(self, title: str) -> None:
+        self._items = [item for item in self._items if item[1] != title]
+    def get_list(self) -> list[Ingredient]:
+        uni_ingredient_to_quantity = {}
+        for ingredient, recipe_title in self._items:
+            key = (ingredient.name, ingredient.unit)
+            if key in uni_ingredient_to_quantity:
+                uni_ingredient_to_quantity[key] += ingredient.quantity
+            else:
+                uni_ingredient_to_quantity[key] = ingredient.quantity
+        result = []
+        for key in uni_ingredient_to_quantity:
+            result.append(
+                Ingredient(
+                    key[0],
+                    uni_ingredient_to_quantity[key],
+                    key[1]
+                )
+            )
+        result.sort(key=lambda x: x.name)
+        return result
+    def __add__(self, other: object) -> 'ShoppingList':
+        if not isinstance(other, ShoppingList):
+            raise TypeError("Можно складывать только ShoppingList")
+        result = ShoppingList()
+        result._items = self._items + other._items
+        return result
